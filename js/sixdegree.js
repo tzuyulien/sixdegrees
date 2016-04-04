058457a9c986b53af9b352e4ab5ab701a140d4c1
tzuyulien <angie981930@gmail.com>
2016-04-04 17:50:56 +0800

    add all

---
+++ js/sixdegree.js
@@ -0,0 +1,83 @@
+$(window).load(function(){
+  // showPageInfo();
+  var windowTop = $(window).scrollTop();
+  var contentWords = $('.content');
+
+  if (contentWords.length) {
+    // var boxTop = contentWords.offset().top;
+    var boxTop = 500;
+    console.log(boxTop);
+    var flag = false;
+
+    $(window).scroll(function() {
+      windowTop = $(window).scrollTop();
+      // console.log(windowTop);
+
+      if (!flag) {
+        if (windowTop >= boxTop) {
+          $('.navi, .content-float, .navi-fixed').addClass('fixed');
+          $('.navi-fixed').show();
+          flag = true;
+        }
+      }
+      else {
+        if (windowTop < boxTop) {
+          $('.navi, .content-float, .navi-fixed').removeClass('fixed');
+          $('.navi-fixed').hide();
+          flag = false;
+        }
+      }
+
+      if (windowTop >= boxTop) {
+        $('.content-float').each(function(i) {
+            if ($(this).position().top <= windowTop + 70) {
+                $('.navi-fixed ul a.active').removeClass('active');
+                $('.navi-fixed ul a').eq(i).addClass('active');
+            }
+
+        });
+      } else {
+          // $('.product-list ul a.active').removeClass('active');
+      }
+
+
+    }).scroll();
+  }
+  
+});
+
+
+
+
+var main = function() {
+
+
+
+  $(".all .navi ul a.tab1").click(function() {
+    $('html, body').animate({scrollTop: $("#tab1-item").offset().top}, 1000);
+    $(this).siblings().removeClass('active');
+    $(this).addClass('active');
+    console.log('hi');
+  });
+  $(".all .navi ul a.tab2").click(function() {
+    $('html, body').animate({scrollTop: $("#tab2-item").offset().top}, 1000);
+    $(this).siblings().removeClass('active');
+    $(this).addClass('active');
+  });
+  $(".all .navi ul a.tab3").click(function() {
+    $('html, body').animate({scrollTop: $("#tab3-item").offset().top}, 1000);
+    $(this).siblings().removeClass('active');
+    $(this).addClass('active');
+  });
+  $(".all .navi ul a.tab4").click(function() {
+    $('html, body').animate({scrollTop: $("#tab4-item").offset().top}, 1000);
+    $(this).siblings().removeClass('active');
+    $(this).addClass('active');
+  });
+  $(".all .navi ul a.tab5").click(function() {
+    $('html, body').animate({scrollTop: $("#tab5-item").offset().top}, 1000);
+    $(this).siblings().removeClass('active');
+    $(this).addClass('active');
+  });
+
+}
